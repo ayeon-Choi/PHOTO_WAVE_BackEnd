10d74cb1bdddf8001ff8fe60a3417bacf6243283
ayeonChoi <201120cay@dsm.hs.kr>
2022-04-21 14:25:04 +0900

    :card_file_box: ::userEntity 추가(#2)

---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import { Album } from "src/album/ablum.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("user")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 45 })
+  email: string;
+
+  @Column({ length: 256 })
+  password: string;
+
+  @ManyToOne(() => Album, (album) => album.id, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "album_id" })
+  album: Album;
+}
